037ad222ff4b8d19b95b2fe668d6ee8537fca4c6
Arshidin <osm.arshidin@gmail.com>
2024-10-05 19:57:24 +0500

    improve authorization system

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use App\Facades\AuthFacade as Auth;
 
 class LoginController extends Controller
 {
     public function index(): View
     {
         return view('auth.login');
+    }
+
+    public function login(LoginRequest $request): RedirectResponse
+    {
+        return Auth::login($request);
     }
 }
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Facades\AuthFacade as Auth;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
 
 class RegisterController extends Controller
 {
     public function index(): View
     {
         return view('auth.register');
+    }
+
+    public function register(RegisterRequest $request): RedirectResponse
+    {
+        return Auth::register($request);
     }
 }
